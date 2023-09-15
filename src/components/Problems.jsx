8ae1981b4f15363ade5ae350234e06a3ad64b60e
imdebamrita <imdebamrita@gmail.com>
2023-09-16 03:48:57 +0530

    error fix

--- src/components/Problems.jsx
+++ src/components/Problems.jsx
@@ -6,7 +6,7 @@ export default function Problems() {
   return (
     <div
       id="problems_section"
-      className="flex flex-col sm:h-screen h-[calc(24s0vh)] justify-center items-center text-white"
+      className="flex flex-col sm:h-screen h-[calc(240vh)] justify-center items-center text-white"
     >
       <h2 className="text-4xl font-bold my-16 sm:mt-24 mb-8">
         Problem Statements
